fb51d5dff57f96af292b981159de9c507d48fbc1
PauloAGR <parzival.pg@gmail.com>
2021-10-22 23:24:41 -0500

    Minor changes
    Spring Security: Acceso a componentes y credenciales actualizadas

--- src/main/java/co/edu/unbosque/tiendagenerica/utils/BasicConfiguration.java
+++ src/main/java/co/edu/unbosque/tiendagenerica/utils/BasicConfiguration.java
@@ -8,18 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class BasicConfiguration extends WebSecurityConfigurerAdapter {
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			
-			.authorizeRequests()
-			.antMatchers("/", "index", "/css/*", "/js/*", "registrar", "login").permitAll()
-			.anyRequest()
-			.authenticated()
-			.and()
-			.formLogin()//.loginPage("/login.html").permitAll()
-			.defaultSuccessUrl("/home.html",true)
-		    .and().csrf().disable();
-}
+
+				.authorizeRequests().antMatchers("/", "index", "/css/*", "/js/*", "/img/*", "registrar", "login").permitAll()
+				.anyRequest().authenticated().and().formLogin()// .loginPage("/login.html").permitAll()
+				.defaultSuccessUrl("/home.html", true).and().csrf().disable();
+	}
 }

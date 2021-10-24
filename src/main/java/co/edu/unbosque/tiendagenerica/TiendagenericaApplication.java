f17caefb25e65f816f502fc1577492927f992fa2
PauloAGR <parzival.pg@gmail.com>
2021-10-23 21:23:50 -0500

    add default route

--- src/main/java/co/edu/unbosque/tiendagenerica/TiendagenericaApplication.java
+++ src/main/java/co/edu/unbosque/tiendagenerica/TiendagenericaApplication.java
@@ -3,10 +3,18 @@ package co.edu.unbosque.tiendagenerica;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class TiendagenericaApplication {
 
+	@RequestMapping("/")
+    //@ResponseBody
+    String home() {
+      return "index.html";
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TiendagenericaApplication.class, args);
 	}
